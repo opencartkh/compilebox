013a61de99ad40b0f3a845a8650dafc8be1a1366
Samir Meghani <smeghani@alum.mit.edu>
2017-04-08 16:01:08 -0700

    reorganize for EB

--- Setup/Install_Cent6.sh
+++ Setup/Install_Cent6.sh
@@ -21,7 +21,7 @@ echo "NodeJS Setup Complete"
 # Start Docker
 ############################
 chmod 777 ../API/DockerTimeout.sh
-chmod 777 ../API/Payload/script.h
+chmod 777 ../API/Payload/script.sh
 chmod 777 ../API/Payload/javaRunner.sh
 chmod 777 UpdateDocker.sh
 
