44df129cd5f8a1866b4c4b6b872743e0e478108c
Samir Meghani <smeghani@alum.mit.edu>
2017-11-19 00:51:52 -0500

    update request queuing system

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express');
 var arr = require('./compilers');
 var sandBox = require('./DockerSandbox');
 var app = express.createServer();
-var port=process.env.PORT;
+var port=process.env.PORT || 3000;
 
 
 var ExpressBrute = require('express-brute');
@@ -37,9 +37,87 @@ function random(size) {
     return require("crypto").randomBytes(size).toString('hex');
 }
 
+var runningRequests = [];
+var MAX_CONCURRENT_REQUESTS = 2;
+var MAX_QUEUE_SIZE = 20;
+
+function shouldDelay(req) {
+    for (var i = 0; i < MAX_CONCURRENT_REQUESTS; i++) {
+	if (runningRequests[i][0] == req) {
+	    return false;
+        }
+    }
+    return true;    
+}
+
+function cleanupQueue() {
+    // throw out anything more than two minutes old
+    var currTime = new Date();
+    for (var i = runningRequests.length - 1; i >= 0; i--) {
+	if ((currTime - runningRequests[1]) > 120000) {
+	    runningRequests.splice(i, 1)
+        }
+    } 
+}
+
+function addToQueue(req, res) {
+    cleanupQueue();
+    if (runningRequests.length <= MAX_QUEUE_SIZE) {
+	console.log('queuing request');
+      runningRequests.push([req, new Date()]);
+      return true;
+    }
+    else {
+	console.log('ignoring request');
+       res.writeHead(500);
+       res.end('queued reached max size');
+       return false;
+    }
+}
+
+function removeFromQueue(req) {
+    for (var i = runningRequests.length - 1; i >= 0; i--) {
+	if (runningRequests[i][0] == req) {
+	    runningRequests.splice(i, 1)
+        }
+    }     
+}
+
+function isQueued(req) {
+	for (var i = 0; i < runningRequests.length; i++) {
+	    if (runningRequests[i][0] == req) {
+		return true;
+	    }
+	}
+        return false;
+}
+
+function runSandbox(req, res, language, code, sandboxType) {
+    if (!isQueued(req)) {
+	res.writeHead(500);
+        res.end('execution timed out');
+	return undefined;
+    }
+    if (shouldDelay(req)) {
+	setTimeout(function() { runSandbox(req, res, language, code, sandboxType); }, 100);
+    }
+    else {
+	sandboxType.run(function(data,exec_time,err)
+			{
+			    //console.log("Data: received: "+ data)
+			    res.send({output:data, langid: language,code:code, errors:err, time:exec_time});
+                            removeFromQueue(req); 
+			});
+    }
+}
 
 app.post('/compile', function(req, res) {
-
+	console.log('compiling');
+    var added = addToQueue(req, res);
+    if (!added) {
+	return;
+    }
+    console.log('added to queue')
     var language = (req.body || []).language;
     var code = (req.body || []).code;
     var stdin = (req.body || []).stdin;
@@ -58,7 +136,7 @@ app.post('/compile', function(req, res) {
         
         return undefined;
     }
-   
+
     var folder= 'temp/' + random(10); //folder in which the temporary folder will be saved
     var path=__dirname+"/"; //current working path
     var vm_name='virtual_machine'; //name of virtual machine that we want to execute
@@ -67,17 +145,21 @@ app.post('/compile', function(req, res) {
     //details of this are present in DockerSandbox.js
     var sandboxType = new sandBox(timeout_value,path,folder,vm_name,arr.compilerArray[language][0],arr.compilerArray[language][1],code,arr.compilerArray[language][2],arr.compilerArray[language][3],arr.compilerArray[language][4],stdin);
 
-
-    //data will contain the output of the compiled/interpreted code
-    //the result maybe normal program output, list of error messages or a Timeout error
-    sandboxType.run(function(data,exec_time,err)
-    {
-        //console.log("Data: received: "+ data)
-    	res.send({output:data, langid: language,code:code, errors:err, time:exec_time});
-    });
-   
+    runSandbox(req, res, language, code, sandboxType);
 });
 
+app.get('/health', function(req, res) 
+{
+    cleanupQueue()
+    if (runningRequests.length >= MAX_QUEUE_SIZE) {
+	res.writeHead(500);
+        res.end('queued reached max size')
+    }
+    else {
+	res.writeHead(200);
+        res.end('healthy');
+    }
+});
 
 app.get('/', function(req, res) 
 {
