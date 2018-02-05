4e410c8ab06e61cbce8fab0fb258089d879551bb
Will Tennien Murphy <will.t.murphy@gmail.com>
2018-02-04 19:31:51 -0500

    sql starting data

---
+++ starting-data.sql
@@ -0,0 +1,36 @@
+
+users
+
+photos
+
+tags
+
+friends
+
+id, first_name, last_name, birthday
+1, Grace Hopper
+2, Brian Kernighan
+3, Ada Lovelace
+4, Ida Rhodes
+5, Dennis Ritchie
+6, Alan Turing
+
+photos
+
+user_id, name
+
+1, Trees
+2, House
+3, Apple
+4, Dog
+5, Cat
+6, Party
+
+friends
+
+Brian Kernighan - Dennis Ritchie
+Alan Turing - Grace Hopper
+Brian Kernighan - Barbara Liskov
+
+tags
+
